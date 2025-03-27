67a94c068d440be6f501f77ed9da4fa54cdb21cc
emersoncotter <ecotterplymouth@gmail.com>
2025-03-27 15:04:09 -0500

    landing init updates

--- index.php
+++ index.php
@@ -16,44 +16,22 @@ if(session_status() !== PHP_SESSION_ACTIVE) session_start();
       include('shared/header.php'); 
     ?>
 
-    <!-- Header -->
-    <div class="header">
-      <h1>My Website</h1>
-      <p>With a <b>flexible</b> layout.</p>
-    </div>
-
-    <!-- The flexible grid (content) -->
-    <div class="row">
-      <div class="side">
-        <h2>About Me</h2>
-        <h5>Photo of me:</h5>
-        <div class="fakeimg" style="height:200px;">Image</div>
-        <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-        <h3>More Text</h3>
-        <p>Lorem ipsum dolor sit ame.</p>
-        <div class="fakeimg" style="height:60px;">Image</div><br>
-        <div class="fakeimg" style="height:60px;">Image</div><br>
-        <div class="fakeimg" style="height:60px;">Image</div>
+    <!-- Main Content -->
+    <div class="main">
+      <img src="media/dumbbell.png" class="background-blur" />
+      
+      <div class="item">
+        <h1>Gamify Your <br> Fitness Journey</h1>
       </div>
-      <div class="main">
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Dec 7, 2024</h5>
-        <div class="fakeimg" style="height:200px;">Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        <br>
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Sep 2, 2024</h5>
-        <div class="fakeimg" style="height:200px;">Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <div class="item">
+        <p>Track your workouts, earn acheivements, and level up your fitness with our gamified workout platform.</p>
+      </div>
+      <div class="item">
+          <a class="button" href="signup.php">Get Started</a>
       </div>
     </div>
 
-    <!-- Footer -->
-    <div class="footer">
-      <h2>Footer</h2>
-    </div>
+ 
 
   </body>
 </html>
--- shared/header.php
+++ shared/header.php
@@ -9,7 +9,9 @@
 	<title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href='https://fonts.googleapis.com/css?family=Inria Sans' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/header-styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
